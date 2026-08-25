5b9bda3bc644a7ec38ebc2e558d512e1904bf010
ayaaatea87 <ayaaatea87@gmail.com>
2026-08-25 18:07:03 +0300

    feat: add basic calculator functionality with user input

---
+++ calculator.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+const rl= readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Enter First Number:",(answer)=>{
+    const firstNumber = Number(answer);
+    rl.question("Enter Second Number:", (answer) =>{
+    const secondNumber = Number(answer);
+        rl.question("Enter operation (+, -, *,/):", (operation) => {
+            switch (operation) {
+                case "+":
+                    console.log(firstNumber + secondNumber);
+                    break;
+
+                case "-":
+                    console.log(firstNumber - secondNumber);
+                    break;
+
+                case "*":
+                    console.log(firstNumber * secondNumber);
+                    break;
+
+                case "/":
+                    console.log(firstNumber / secondNumber);
+                    break;
+
+                default:
+                    console.log("Invalid operation");
+            }
+            rl.close();
+        });
+        
+    });
+});
