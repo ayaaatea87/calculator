5570f8a86e8fb361db07776b4e525a697810786e
ayaaatea87 <ayaaatea87@gmail.com>
2026-08-25 19:03:32 +0300

    feat: allow multiple calculations

--- calculator.js
+++ calculator.js
@@ -3,35 +3,45 @@ const rl= readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question("Enter First Number:",(answer)=>{
-    const firstNumber = Number(answer);
-    rl.question("Enter Second Number:", (answer) =>{
-    const secondNumber = Number(answer);
-        rl.question("Enter operation (+, -, *,/):", (operation) => {
-            switch (operation) {
-                case "+":
-                    console.log(firstNumber + secondNumber);
-                    break;
+function calculate(){
+    rl.question("Enter First Number:", (answer) => {
+        const firstNumber = Number(answer);
+        rl.question("Enter Second Number:", (answer) => {
+            const secondNumber = Number(answer);
+            rl.question("Enter operation (+, -, *,/):", (operation) => {
+                switch (operation) {
+                    case "+":
+                        console.log(firstNumber + secondNumber);
+                        break;
 
-                case "-":
-                    console.log(firstNumber - secondNumber);
-                    break;
+                    case "-":
+                        console.log(firstNumber - secondNumber);
+                        break;
 
-                case "*":
-                    console.log(firstNumber * secondNumber);
-                    break;
+                    case "*":
+                        console.log(firstNumber * secondNumber);
+                        break;
 
-                case "/":
-                    if (secondNumber===0){
-                        console.log("Error:Division by zero is not allowed");
-                    } else { console.log(firstNumber / secondNumber); }
-                    break;
+                    case "/":
+                        if (secondNumber === 0) {
+                            console.log("Error:Division by zero is not allowed");
+                        } else { console.log(firstNumber / secondNumber); }
+                        break;
 
-                default:
-                    console.log("Invalid operation");
-            }
-            rl.close();
+                    default:
+                        console.log("Invalid operation");
+                }
+                rl.question("do you want to perform another calculation? (yes/no):", answer =>{
+                    if(answer.toLowerCase()== "yes"){
+                        calculate();
+                    }else{
+                        console.log("Thanks for using our calculation service ..!");
+                        rl.close();
+                    }
+                });
+            });
         });
-        
     });
-});
+
+}
+calculate();
